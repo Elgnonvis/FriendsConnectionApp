4cedccfefba629244f7e7f2666e554eac50d056e
elgnonvis <elgnonvis@gmail.com>
2021-08-29 10:46:16 +0100

    Generate User model, creating validations and unique constraints

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+    #has_many :blogs
+    #has_secure_password
+
+    validates :name, presence: true, length: {maximum: 30}
+    validates :email, presence: true,
+               uniqueness: true,
+               length: {maximum: 255},
+               format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    validates :password, presence: true,
+               length: {minimum: 8}
+    
+    before_validation { email.downcase! } 
+    
+end
+end
